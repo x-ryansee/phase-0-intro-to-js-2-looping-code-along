24292fd6cc77eb35c13eadb123699dabf5f4a1a8
Ryan See <ryanjosephsee@gmail.com>
2022-11-03 16:17:15 -0700

    commit tree 4e7984b43a7b3f3385893a88857ea449a031258e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+const people = ["Charlie", "Samip", "Ali"];
+const event = 'birthday';
+for (let i = 0; i < people.length; i++) {
+    console.log(`Thanks ${people[i]} for the wonderful ${event} gift`);
+}
